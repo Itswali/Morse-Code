d8a135ab638ed6cce10a9892cf2c7538965d7031
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-03-07 13:06:24 -0600

    call the morse function

--- morse.rb
+++ morse.rb
@@ -16,3 +16,10 @@ def single_word(morse_word)
   msg_word += ' '
 end
 
+def decode(morse_code)
+  morse_code.split('   ').each do |word|
+    print single_word(word)
+  end
+end
+
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
